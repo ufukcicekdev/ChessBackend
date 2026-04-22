ef457c63005b80d6ffb03817a907c76d079c1bd4
EAGLEEARTH <ufukcicek199@gmail.com>
2026-04-22 22:13:29 +0300

    push

--- config/asgi.py
+++ config/asgi.py
@@ -1,11 +1,13 @@
 import os
+
+# Must be set before importing anything that touches Django settings.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from apps.chess.middleware import JWTAuthMiddlewareStack
 import apps.chess.routing as chess_routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
