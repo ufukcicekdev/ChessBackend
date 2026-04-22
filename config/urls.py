d477e5872151de2563efc9f1147544c60bc99d83
EAGLEEARTH <ufukcicek199@gmail.com>
2026-04-22 22:18:58 +0300

    push

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", lambda request: JsonResponse({"status": "ok"})),
     path("healthz/", lambda request: JsonResponse({"status": "ok"})),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
